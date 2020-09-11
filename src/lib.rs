0b3d997b30197dda5ed8b2038e8fb5d01e71d867
Craig McQueen <craig.mcqueen@innerrange.com>
2020-09-11 17:12:14 +1000

    Encode/decode functions return Result; decode function handle length code FF
    Returning Result allows for errors to be returned as needed.
    
    The decode functions previously weren't handling the special length code 0xFF, used for encoding long strings of non-zero bytes.

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,55 @@
 
 #![allow(dead_code)]
 
+#[derive(Debug, PartialEq)]
+pub enum Error {
+    OutputBufferFull,
+    ZeroInEncodedData,
+    TruncatedEncodedData,
+}
+impl std::error::Error for Error {}
+impl std::fmt::Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        match *self {
+            Error::OutputBufferFull => {
+                write!(f, "Output buffer is full")
+            }
+            Error::ZeroInEncodedData => {
+                write!(f, "Zero found in encoded input data")
+            }
+            Error::TruncatedEncodedData => {
+                write!(f, "Unexpected end of encoded input data")
+            }
+        }
+    }
+}
+
+pub type Result<T> = std::result::Result<T, crate::Error>;
+
 pub mod cobs {
-    pub fn encode<'a>(out_buf: &'a mut [u8], in_buf: &[u8]) -> &'a[u8] {
+    pub fn encode<'a>(out_buf: &'a mut [u8], in_buf: &[u8]) -> crate::Result<&'a[u8]> {
         let mut code_i = 0;
         let mut out_i = 1;
 
         for x in in_buf {
             if *x == 0 {
+                if code_i >= out_buf.len() {
+                    return Err(crate::Error::OutputBufferFull);
+                }
                 out_buf[code_i] = (out_i - code_i) as u8;
                 code_i = out_i;
                 out_i = code_i + 1;
             }
             else {
+                if out_i >= out_buf.len() {
+                    return Err(crate::Error::OutputBufferFull);
+                }
                 out_buf[out_i] = *x;
                 out_i += 1;
                 if out_i - code_i >= 0xFE {
+                    if code_i >= out_buf.len() {
+                        return Err(crate::Error::OutputBufferFull);
+                    }
                     out_buf[code_i] = 0xFF;
                     code_i = out_i;
                     out_i = code_i + 1;
@@ -23,49 +57,71 @@ pub mod cobs {
             }
         }
 
-        /* We've reached the end of the source data.
-         * Finalise the remaining output. In particular, write the code (length) byte.
-         * Update the pointer to calculate the final output length.
-         */
+        // We've reached the end of the source data.
+        // Finalise the remaining output. In particular, write the code (length) byte.
+        // Update the pointer to calculate the final output length.
+        if code_i >= out_buf.len() {
+            return Err(crate::Error::OutputBufferFull);
+        }
         out_buf[code_i] = (out_i - code_i) as u8;
 
-        &out_buf[..out_i]
+        Ok(&out_buf[..out_i])
     }
 
-    pub fn decode<'a>(out_buf: &'a mut [u8], in_buf: &[u8]) -> &'a[u8] {
+    pub fn decode<'a>(out_buf: &'a mut [u8], in_buf: &[u8]) -> crate::Result<&'a[u8]> {
         let mut code_i = 0;
         let mut out_i = 0;
 
         if in_buf.len() != 0 {
             loop {
                 let code = in_buf[code_i];
+                if code == 0 {
+                    return Err(crate::Error::ZeroInEncodedData);
+                }
                 for in_i in (code_i + 1)..(code_i + code as usize) {
                     if in_i >= in_buf.len() {
-                        break;
+                        return Err(crate::Error::TruncatedEncodedData);
                     }
-                    out_buf[out_i] = in_buf[in_i];
+                    let in_byte = in_buf[in_i];
+                    if in_byte == 0 {
+                        return Err(crate::Error::ZeroInEncodedData);
+                    }
+                    if out_i >= out_buf.len() {
+                        return Err(crate::Error::OutputBufferFull);
+                    }
+                    out_buf[out_i] = in_byte;
                     out_i += 1;
                 }
                 code_i += code as usize;
                 if code_i >= in_buf.len() {
+                    // End of data. Exit, without outputting a trailing zero for the end of the data.
                     break;
                 }
-                out_buf[out_i] = 0;
-                out_i += 1;
+                if code < 0xFF {
+                    // Output trailing zero.
+                    if out_i >= out_buf.len() {
+                        return Err(crate::Error::OutputBufferFull);
+                    }
+                    out_buf[out_i] = 0;
+                    out_i += 1;
+                }
             }
         }
-        &out_buf[..out_i]
+        Ok(&out_buf[..out_i])
     }
 }
 
 pub mod cobsr {
-    pub fn encode<'a>(out_buf: &'a mut [u8], in_buf: &[u8]) -> &'a[u8] {
+    pub fn encode<'a>(out_buf: &'a mut [u8], in_buf: &[u8]) -> crate::Result<&'a[u8]> {
         let mut code_i = 0;
         let mut out_i = 1;
         let mut last_value = 0u8;
 
         for x in in_buf {
             if *x == 0 {
+                if code_i >= out_buf.len() {
+                    return Err(crate::Error::OutputBufferFull);
+                }
                 out_buf[code_i] = (out_i - code_i) as u8;
                 code_i = out_i;
                 out_i = code_i + 1;
@@ -73,9 +129,15 @@ pub mod cobsr {
             }
             else {
                 last_value = *x;
+                if out_i >= out_buf.len() {
+                    return Err(crate::Error::OutputBufferFull);
+                }
                 out_buf[out_i] = last_value;
                 out_i += 1;
                 if out_i - code_i >= 0xFE {
+                    if code_i >= out_buf.len() {
+                        return Err(crate::Error::OutputBufferFull);
+                    }
                     out_buf[code_i] = 0xFF;
                     code_i = out_i;
                     out_i = code_i + 1;
@@ -83,10 +145,12 @@ pub mod cobsr {
             }
         }
 
-        /* We've reached the end of the source data.
-         * Finalise the remaining output. In particular, write the code (length) byte.
-         * Update the pointer to calculate the final output length.
-         */
+        // We've reached the end of the source data.
+        // Finalise the remaining output. In particular, write the code (length) byte.
+        // Update the pointer to calculate the final output length.
+        if code_i >= out_buf.len() {
+            return Err(crate::Error::OutputBufferFull);
+        }
         if last_value >= (out_i - code_i) as u8 {
             out_buf[code_i] = last_value;
             out_i -= 1;
@@ -95,33 +159,55 @@ pub mod cobsr {
             out_buf[code_i] = (out_i - code_i) as u8;
         }
 
-        &out_buf[..out_i]
+        Ok(&out_buf[..out_i])
     }
 
-    pub fn decode<'a>(out_buf: &'a mut [u8], in_buf: &[u8]) -> &'a[u8] {
+    pub fn decode<'a>(out_buf: &'a mut [u8], in_buf: &[u8]) -> crate::Result<&'a[u8]> {
         let mut code_i = 0;
         let mut out_i = 0;
 
         if in_buf.len() != 0 {
             loop {
                 let code = in_buf[code_i];
+                if code == 0 {
+                    return Err(crate::Error::ZeroInEncodedData);
+                }
                 for in_i in (code_i + 1)..(code_i + code as usize) {
                     if in_i >= in_buf.len() {
+                        // End of data, where length code is greater than remaining data.
+                        // Output the length code as the last output byte.
+                        if out_i >= out_buf.len() {
+                            return Err(crate::Error::OutputBufferFull);
+                        }
                         out_buf[out_i] = code;
                         out_i += 1;
                         break;
                     }
-                    out_buf[out_i] = in_buf[in_i];
+                    let in_byte = in_buf[in_i];
+                    if in_byte == 0 {
+                        return Err(crate::Error::ZeroInEncodedData);
+                    }
+                    if out_i >= out_buf.len() {
+                        return Err(crate::Error::OutputBufferFull);
+                    }
+                    out_buf[out_i] = in_byte;
                     out_i += 1;
                 }
                 code_i += code as usize;
                 if code_i >= in_buf.len() {
+                    // End of data. Exit, without outputting a trailing zero for the end of the data.
                     break;
                 }
-                out_buf[out_i] = 0;
-                out_i += 1;
+                if code < 0xFF {
+                    // Output trailing zero.
+                    if out_i >= out_buf.len() {
+                        return Err(crate::Error::OutputBufferFull);
+                    }
+                    out_buf[out_i] = 0;
+                    out_i += 1;
+                }
             }
         }
-        &out_buf[..out_i]
+        Ok(&out_buf[..out_i])
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,41 @@
 #![allow(dead_code)]
 
-fn main() {
-    let mut cobs_buf: [u8; 1000] = [ 0; 1000 ];
-    let mut cobs_decode_buf: [u8; 1000] = [ 0xCC; 1000 ];
+use std::error::Error;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let mut cobs_buf = [ 0x55_u8; 1000 ];
+    let mut cobs_decode_buf = [ 0xCC_u8; 1000 ];
     let data = b"AAA\x05\x00CCC\x06";
-    let bad_cobs_encoded_data = b"\x05AAA";
 
-    let data_cobs = cobs::cobs::encode(&mut cobs_buf, data);
-    println!("COBS encode: {:X?}", data_cobs);
+    {
+        let data_cobs = cobs::cobs::encode(&mut cobs_buf, data)?;
+        println!("COBS encode: {:X?}", data_cobs);
+        let data_cobs_decoded = cobs::cobs::decode(&mut cobs_decode_buf, data_cobs)?;
+        println!("COBS decode: {:X?}", data_cobs_decoded);
+    }
 
-    let data_cobs_decoded = cobs::cobs::decode(&mut cobs_decode_buf, data_cobs);
-    println!("COBS decode: {:X?}", data_cobs_decoded);
+    // Deliberately try decoding bad data.
+    {
+        let bad_cobs_encoded_data = b"\x00sAAA";
+        let result = cobs::cobs::decode(&mut cobs_decode_buf, bad_cobs_encoded_data);
+        assert!(result.is_err());
+        assert_eq!(result.err(), Some(cobs::Error::ZeroInEncodedData));
 
-    let data_cobs_decoded = cobs::cobs::decode(&mut cobs_decode_buf, bad_cobs_encoded_data);
-    println!("COBS decode bad data: {:X?}", data_cobs_decoded);
+        let bad_cobs_encoded_data = b"\x05AAA";
+        let result = cobs::cobs::decode(&mut cobs_decode_buf, bad_cobs_encoded_data);
+        assert!(result.is_err());
+        assert_eq!(result.err(), Some(cobs::Error::TruncatedEncodedData));
+    }
 
     // Now COBSR/R.
-    println!("");
-    let data_cobs = cobs::cobsr::encode(&mut cobs_buf, data);
-    println!("COBS/R encode: {:X?}", data_cobs);
+    {
+        println!("");
+        let data_cobs = cobs::cobsr::encode(&mut cobs_buf, data)?;
+        println!("COBS/R encode: {:X?}", data_cobs);
 
-    let data_cobs_decoded = cobs::cobsr::decode(&mut cobs_decode_buf, data_cobs);
-    println!("COBS/R decode: {:X?}", data_cobs_decoded);
+        let data_cobs_decoded = cobs::cobsr::decode(&mut cobs_decode_buf, data_cobs)?;
+        println!("COBS/R decode: {:X?}", data_cobs_decoded);
+    }
+
+    Ok(())
 }
