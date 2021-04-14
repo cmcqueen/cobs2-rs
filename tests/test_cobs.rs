4a921ed0024f8da5ee79b7b0988dbbecff3aceb1
Craig McQueen <craig.mcqueen@innerrange.com>
2021-04-14 11:17:52 +1000

    Add some more test data
    A few of these currently fail. To be addressed.

--- tests/test_cobs.rs
+++ tests/test_cobs.rs
@@ -1,36 +1,53 @@
 use ::cobs::{cobs, cobsr};
 
 struct DataEncodedMapping<'a> {
-    pub data: &'a [u8],
+    pub rawdata: &'a [u8],
     pub encoded: &'a [u8],
 }
 
-const PREDEFINED_ENCODINGS: [DataEncodedMapping; 9] = [
-    DataEncodedMapping{ data: b"",                      encoded: b"\x01"                        },
-    DataEncodedMapping{ data: b"1",                     encoded: b"\x021"                       },
-    DataEncodedMapping{ data: b"12345",                 encoded: b"\x0612345"                   },
-    DataEncodedMapping{ data: b"12345\x006789",         encoded: b"\x0612345\x056789"           },
-    DataEncodedMapping{ data: b"\x0012345\x006789",     encoded: b"\x01\x0612345\x056789"       },
-    DataEncodedMapping{ data: b"12345\x006789\x00",     encoded: b"\x0612345\x056789\x01"       },
-    DataEncodedMapping{ data: b"\x00",                  encoded: b"\x01\x01"                    },
-    DataEncodedMapping{ data: b"\x00\x00",              encoded: b"\x01\x01\x01"                },
-    DataEncodedMapping{ data: b"\x00\x00\x00",          encoded: b"\x01\x01\x01\x01"            },
+const PREDEFINED_ENCODINGS: [DataEncodedMapping; 13] = [
+    DataEncodedMapping { rawdata: b"",                      encoded: b"\x01"                        },
+    DataEncodedMapping { rawdata: b"1",                     encoded: b"\x021"                       },
+    DataEncodedMapping { rawdata: b"12345",                 encoded: b"\x0612345"                   },
+    DataEncodedMapping { rawdata: b"12345\x006789",         encoded: b"\x0612345\x056789"           },
+    DataEncodedMapping { rawdata: b"\x0012345\x006789",     encoded: b"\x01\x0612345\x056789"       },
+    DataEncodedMapping { rawdata: b"12345\x006789\x00",     encoded: b"\x0612345\x056789\x01"       },
+    DataEncodedMapping { rawdata: b"\x00",                  encoded: b"\x01\x01"                    },
+    DataEncodedMapping { rawdata: b"\x00\x00",              encoded: b"\x01\x01\x01"                },
+    DataEncodedMapping { rawdata: b"\x00\x00\x00",          encoded: b"\x01\x01\x01\x01"            },
+    DataEncodedMapping {
+        rawdata: b"0123456789ABCDEFGHIJKLMNOPQRSTabcdefghijklmnopqrst0123456789ABCDEFGHIJKLMNOPQRSTabcdefghijklmnopqrst0123456789ABCDEFGHIJKLMNOPQRSTabcdefghijklmnopqrst0123456789ABCDEFGHIJKLMNOPQRSTabcdefghijklmnopqrst0123456789ABCDEFGHIJKLMNOPQRSTabcdefghijklmnopqrst123",
+        encoded: b"\xFE0123456789ABCDEFGHIJKLMNOPQRSTabcdefghijklmnopqrst0123456789ABCDEFGHIJKLMNOPQRSTabcdefghijklmnopqrst0123456789ABCDEFGHIJKLMNOPQRSTabcdefghijklmnopqrst0123456789ABCDEFGHIJKLMNOPQRSTabcdefghijklmnopqrst0123456789ABCDEFGHIJKLMNOPQRSTabcdefghijklmnopqrst123",
+    },
+    // Currently fails
+    DataEncodedMapping {
+        rawdata: b"0123456789ABCDEFGHIJKLMNOPQRSTabcdefghijklmnopqrst0123456789ABCDEFGHIJKLMNOPQRSTabcdefghijklmnopqrst0123456789ABCDEFGHIJKLMNOPQRSTabcdefghijklmnopqrst0123456789ABCDEFGHIJKLMNOPQRSTabcdefghijklmnopqrst0123456789ABCDEFGHIJKLMNOPQRSTabcdefghijklmnopqrst1234",
+        encoded: b"\xFF0123456789ABCDEFGHIJKLMNOPQRSTabcdefghijklmnopqrst0123456789ABCDEFGHIJKLMNOPQRSTabcdefghijklmnopqrst0123456789ABCDEFGHIJKLMNOPQRSTabcdefghijklmnopqrst0123456789ABCDEFGHIJKLMNOPQRSTabcdefghijklmnopqrst0123456789ABCDEFGHIJKLMNOPQRSTabcdefghijklmnopqrst1234",
+    },
+    DataEncodedMapping {
+        rawdata: b"0123456789ABCDEFGHIJKLMNOPQRSTabcdefghijklmnopqrst0123456789ABCDEFGHIJKLMNOPQRSTabcdefghijklmnopqrst0123456789ABCDEFGHIJKLMNOPQRSTabcdefghijklmnopqrst0123456789ABCDEFGHIJKLMNOPQRSTabcdefghijklmnopqrst0123456789ABCDEFGHIJKLMNOPQRSTabcdefghijklmnopqrst12345",
+        encoded: b"\xFF0123456789ABCDEFGHIJKLMNOPQRSTabcdefghijklmnopqrst0123456789ABCDEFGHIJKLMNOPQRSTabcdefghijklmnopqrst0123456789ABCDEFGHIJKLMNOPQRSTabcdefghijklmnopqrst0123456789ABCDEFGHIJKLMNOPQRSTabcdefghijklmnopqrst0123456789ABCDEFGHIJKLMNOPQRSTabcdefghijklmnopqrst1234\x025",
+    },
+    DataEncodedMapping {
+        rawdata: b"\x000123456789ABCDEFGHIJKLMNOPQRSTabcdefghijklmnopqrst0123456789ABCDEFGHIJKLMNOPQRSTabcdefghijklmnopqrst0123456789ABCDEFGHIJKLMNOPQRSTabcdefghijklmnopqrst0123456789ABCDEFGHIJKLMNOPQRSTabcdefghijklmnopqrst0123456789ABCDEFGHIJKLMNOPQRSTabcdefghijklmnopqrst12345",
+        encoded: b"\x01\xFF0123456789ABCDEFGHIJKLMNOPQRSTabcdefghijklmnopqrst0123456789ABCDEFGHIJKLMNOPQRSTabcdefghijklmnopqrst0123456789ABCDEFGHIJKLMNOPQRSTabcdefghijklmnopqrst0123456789ABCDEFGHIJKLMNOPQRSTabcdefghijklmnopqrst0123456789ABCDEFGHIJKLMNOPQRSTabcdefghijklmnopqrst1234\x025",
+    },
 ];
 
 #[test]
 fn test_cobs_vector_predefined() {
     for mapping in PREDEFINED_ENCODINGS.iter() {
-        let enc_result = cobs::encode_vector(mapping.data);
+        let enc_result = cobs::encode_vector(mapping.rawdata);
         assert!(enc_result.is_ok());
         assert_eq!(&enc_result.clone().unwrap(), mapping.encoded);
 
         let dec_result = cobs::decode_vector(&enc_result.clone().unwrap());
         assert!(dec_result.is_ok());
-        assert_eq!(dec_result.unwrap(), mapping.data);
+        assert_eq!(dec_result.unwrap(), mapping.rawdata);
 
-        // COBS/R decode function should also be able to decode COBS-encoded data.
+        // COBS/R decode function should also be able to decode COBS-encoded rawdata.
         let dec_result = cobsr::decode_vector(&enc_result.unwrap());
         assert!(dec_result.is_ok());
-        assert_eq!(dec_result.unwrap(), mapping.data);
+        assert_eq!(dec_result.unwrap(), mapping.rawdata);
     }
 }
--- tests/test_cobsr.rs
+++ tests/test_cobsr.rs
@@ -1,42 +1,64 @@
 use ::cobs::cobsr;
 
 struct DataEncodedMapping<'a> {
-    pub data: &'a [u8],
+    pub rawdata: &'a [u8],
     pub encoded: &'a [u8],
 }
 
-const PREDEFINED_ENCODINGS: [DataEncodedMapping; 20] = [
-    DataEncodedMapping{ data: b"",                                  encoded: b"\x01"                            },
-    DataEncodedMapping{ data: b"\x01",                              encoded: b"\x02\x01"                        },
-    DataEncodedMapping{ data: b"\x02",                              encoded: b"\x02"                            },
-    DataEncodedMapping{ data: b"\x03",                              encoded: b"\x03"                            },
-    DataEncodedMapping{ data: b"\x7E",                              encoded: b"\x7E"                            },
-    DataEncodedMapping{ data: b"\x7F",                              encoded: b"\x7F"                            },
-    DataEncodedMapping{ data: b"\x80",                              encoded: b"\x80"                            },
-    DataEncodedMapping{ data: b"\xD5",                              encoded: b"\xD5"                            },
-    DataEncodedMapping{ data: b"\xFE",                              encoded: b"\xFE"                            },
-    DataEncodedMapping{ data: b"\xFF",                              encoded: b"\xFF"                            },
-    DataEncodedMapping{ data: b"1",                                 encoded: b"1"                               },
-    DataEncodedMapping{ data: b"\x05\x04\x03\x02\x01",              encoded: b"\x06\x05\x04\x03\x02\x01"        },
-    DataEncodedMapping{ data: b"12345",                             encoded: b"51234"                           },
-    DataEncodedMapping{ data: b"12345\x00\x04\x03\x02\x01",         encoded: b"\x0612345\x05\x04\x03\x02\x01"   },
-    DataEncodedMapping{ data: b"12345\x006789",                     encoded: b"\x06123459678"                   },
-    DataEncodedMapping{ data: b"\x0012345\x006789",                 encoded: b"\x01\x06123459678"               },
-    DataEncodedMapping{ data: b"12345\x006789\x00",                 encoded: b"\x0612345\x056789\x01"           },
-    DataEncodedMapping{ data: b"\x00",                              encoded: b"\x01\x01"                        },
-    DataEncodedMapping{ data: b"\x00\x00",                          encoded: b"\x01\x01\x01"                    },
-    DataEncodedMapping{ data: b"\x00\x00\x00",                      encoded: b"\x01\x01\x01\x01"                },
+const PREDEFINED_ENCODINGS: [DataEncodedMapping; 25] = [
+    DataEncodedMapping{ rawdata: b"",                                   encoded: b"\x01"                            },
+    DataEncodedMapping{ rawdata: b"\x01",                               encoded: b"\x02\x01"                        },
+    DataEncodedMapping{ rawdata: b"\x02",                               encoded: b"\x02"                            },
+    DataEncodedMapping{ rawdata: b"\x03",                               encoded: b"\x03"                            },
+    DataEncodedMapping{ rawdata: b"\x7E",                               encoded: b"\x7E"                            },
+    DataEncodedMapping{ rawdata: b"\x7F",                               encoded: b"\x7F"                            },
+    DataEncodedMapping{ rawdata: b"\x80",                               encoded: b"\x80"                            },
+    DataEncodedMapping{ rawdata: b"\xD5",                               encoded: b"\xD5"                            },
+    DataEncodedMapping{ rawdata: b"\xFE",                               encoded: b"\xFE"                            },
+    DataEncodedMapping{ rawdata: b"\xFF",                               encoded: b"\xFF"                            },
+    DataEncodedMapping{ rawdata: b"1",                                  encoded: b"1"                               },
+    DataEncodedMapping{ rawdata: b"\x05\x04\x03\x02\x01",               encoded: b"\x06\x05\x04\x03\x02\x01"        },
+    DataEncodedMapping{ rawdata: b"12345",                              encoded: b"51234"                           },
+    DataEncodedMapping{ rawdata: b"12345\x00\x04\x03\x02\x01",          encoded: b"\x0612345\x05\x04\x03\x02\x01"   },
+    DataEncodedMapping{ rawdata: b"12345\x006789",                      encoded: b"\x06123459678"                   },
+    DataEncodedMapping{ rawdata: b"\x0012345\x006789",                  encoded: b"\x01\x06123459678"               },
+    DataEncodedMapping{ rawdata: b"12345\x006789\x00",                  encoded: b"\x0612345\x056789\x01"           },
+    DataEncodedMapping{ rawdata: b"\x00",                               encoded: b"\x01\x01"                        },
+    DataEncodedMapping{ rawdata: b"\x00\x00",                           encoded: b"\x01\x01\x01"                    },
+    DataEncodedMapping{ rawdata: b"\x00\x00\x00",                       encoded: b"\x01\x01\x01\x01"                },
+    DataEncodedMapping {
+        rawdata: b"0123456789ABCDEFGHIJKLMNOPQRSTabcdefghijklmnopqrst0123456789ABCDEFGHIJKLMNOPQRSTabcdefghijklmnopqrst0123456789ABCDEFGHIJKLMNOPQRSTabcdefghijklmnopqrst0123456789ABCDEFGHIJKLMNOPQRSTabcdefghijklmnopqrst0123456789ABCDEFGHIJKLMNOPQRSTabcdefghijklmnopqrst123",
+        encoded: b"\xFE0123456789ABCDEFGHIJKLMNOPQRSTabcdefghijklmnopqrst0123456789ABCDEFGHIJKLMNOPQRSTabcdefghijklmnopqrst0123456789ABCDEFGHIJKLMNOPQRSTabcdefghijklmnopqrst0123456789ABCDEFGHIJKLMNOPQRSTabcdefghijklmnopqrst0123456789ABCDEFGHIJKLMNOPQRSTabcdefghijklmnopqrst123",
+    },
+    // Currently fails
+    DataEncodedMapping {
+        rawdata: b"0123456789ABCDEFGHIJKLMNOPQRSTabcdefghijklmnopqrst0123456789ABCDEFGHIJKLMNOPQRSTabcdefghijklmnopqrst0123456789ABCDEFGHIJKLMNOPQRSTabcdefghijklmnopqrst0123456789ABCDEFGHIJKLMNOPQRSTabcdefghijklmnopqrst0123456789ABCDEFGHIJKLMNOPQRSTabcdefghijklmnopqrst1234",
+        encoded: b"\xFF0123456789ABCDEFGHIJKLMNOPQRSTabcdefghijklmnopqrst0123456789ABCDEFGHIJKLMNOPQRSTabcdefghijklmnopqrst0123456789ABCDEFGHIJKLMNOPQRSTabcdefghijklmnopqrst0123456789ABCDEFGHIJKLMNOPQRSTabcdefghijklmnopqrst0123456789ABCDEFGHIJKLMNOPQRSTabcdefghijklmnopqrst1234",
+    },
+    DataEncodedMapping {
+        rawdata: b"0123456789ABCDEFGHIJKLMNOPQRSTabcdefghijklmnopqrst0123456789ABCDEFGHIJKLMNOPQRSTabcdefghijklmnopqrst0123456789ABCDEFGHIJKLMNOPQRSTabcdefghijklmnopqrst0123456789ABCDEFGHIJKLMNOPQRSTabcdefghijklmnopqrst0123456789ABCDEFGHIJKLMNOPQRSTabcdefghijklmnopqrst12345",
+        encoded: b"\xFF0123456789ABCDEFGHIJKLMNOPQRSTabcdefghijklmnopqrst0123456789ABCDEFGHIJKLMNOPQRSTabcdefghijklmnopqrst0123456789ABCDEFGHIJKLMNOPQRSTabcdefghijklmnopqrst0123456789ABCDEFGHIJKLMNOPQRSTabcdefghijklmnopqrst0123456789ABCDEFGHIJKLMNOPQRSTabcdefghijklmnopqrst12345",
+    },
+    DataEncodedMapping {
+        rawdata: b"\x000123456789ABCDEFGHIJKLMNOPQRSTabcdefghijklmnopqrst0123456789ABCDEFGHIJKLMNOPQRSTabcdefghijklmnopqrst0123456789ABCDEFGHIJKLMNOPQRSTabcdefghijklmnopqrst0123456789ABCDEFGHIJKLMNOPQRSTabcdefghijklmnopqrst0123456789ABCDEFGHIJKLMNOPQRSTabcdefghijklmnopqrst12345",
+        encoded: b"\x01\xFF0123456789ABCDEFGHIJKLMNOPQRSTabcdefghijklmnopqrst0123456789ABCDEFGHIJKLMNOPQRSTabcdefghijklmnopqrst0123456789ABCDEFGHIJKLMNOPQRSTabcdefghijklmnopqrst0123456789ABCDEFGHIJKLMNOPQRSTabcdefghijklmnopqrst0123456789ABCDEFGHIJKLMNOPQRSTabcdefghijklmnopqrst12345",
+    },
+    // Currently fails
+    DataEncodedMapping {
+        rawdata: b"0123456789ABCDEFGHIJKLMNOPQRSTabcdefghijklmnopqrst0123456789ABCDEFGHIJKLMNOPQRSTabcdefghijklmnopqrst0123456789ABCDEFGHIJKLMNOPQRSTabcdefghijklmnopqrst0123456789ABCDEFGHIJKLMNOPQRSTabcdefghijklmnopqrst0123456789ABCDEFGHIJKLMNOPQRSTabcdefghijklmnopqrst123\xFF",
+        encoded: b"\xFF0123456789ABCDEFGHIJKLMNOPQRSTabcdefghijklmnopqrst0123456789ABCDEFGHIJKLMNOPQRSTabcdefghijklmnopqrst0123456789ABCDEFGHIJKLMNOPQRSTabcdefghijklmnopqrst0123456789ABCDEFGHIJKLMNOPQRSTabcdefghijklmnopqrst0123456789ABCDEFGHIJKLMNOPQRSTabcdefghijklmnopqrst123",
+    },
 ];
 
 #[test]
 fn test_cobsr_vector_predefined() {
     for mapping in PREDEFINED_ENCODINGS.iter() {
-        let enc_result = cobsr::encode_vector(mapping.data);
+        let enc_result = cobsr::encode_vector(mapping.rawdata);
         assert!(enc_result.is_ok());
         assert_eq!(&enc_result.clone().unwrap(), mapping.encoded);
 
         let dec_result = cobsr::decode_vector(&enc_result.unwrap());
         assert!(dec_result.is_ok());
-        assert_eq!(dec_result.unwrap(), mapping.data);
+        assert_eq!(dec_result.unwrap(), mapping.rawdata);
     }
 }
